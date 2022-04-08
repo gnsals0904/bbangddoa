3f18c44990cc27be8ce99d01f34c4ed427396043
김훈민 <hunmin@gimhunmin-ui-MacBookPro.local>
2022-04-08 20:26:31 +0900

    project setup

---
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bbangddoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangddoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbangddoaApplication.class, args);
+	}
+
+}
