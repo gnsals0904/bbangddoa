a25032c5544a4111048ec7ddd93e7d7627316474
gnsals0904 <gnsals0904@naver.com>
2022-04-18 21:29:25 +0900

    implementing ping pong

--- Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
@@ -5,6 +5,8 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.Compression;
@@ -19,9 +21,9 @@ public class BbangddoaApplication extends ListenerAdapter {
 
 	public static void main(String[] args) throws LoginException {
 		SpringApplication.run(BbangddoaApplication.class, args);
-
-		JDA jda = JDABuilder.createDefault("OTYyMjY2NDM3MTE4OTkyNDQ1.YlFCdA.14pfQyOuH3aqWcZi6Klzvo44akU").build();
-		//You can also add event listeners to the already built JDA instance
+		String bot_token = "OTYyMjY2NDM3MTE4OTkyNDQ1.YlFCdA.14pfQyOuH3aqWcZi6Klzvo44akU";
+		JDA jda = JDABuilder.createDefault(bot_token).build();
+		// You can also add event listeners to the already built JDA instance
 		// Note that some events may not be received if the listener is added after calling build()
 		// This includes events such as the ReadyEvent
 		jda.getPresence().setStatus(OnlineStatus.ONLINE);
@@ -32,6 +34,15 @@ public class BbangddoaApplication extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event)
 	{
+		Message msg = event.getMessage();
+		if (msg.getContentRaw().equals("!ping"))
+		{
+			MessageChannel channel = event.getChannel();
+			long time = System.currentTimeMillis();
+			System.out.println("event :" + msg.getContentRaw());
+			event.getChannel().sendMessage("pong!").queue();
+		}
+
 		if (event.isFromType(ChannelType.PRIVATE))
 		{
 			System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
