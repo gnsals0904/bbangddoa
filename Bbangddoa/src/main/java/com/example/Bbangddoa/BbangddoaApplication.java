8b40889f0e5dfd4c2035c0fc8cea63fecbbe9ec5
gnsals0904 <gnsals0904@naver.com>
2022-05-02 16:30:17 +0900

    Implementing search summoner info function

--- Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
@@ -32,22 +32,57 @@ import java.nio.file.Paths;
 
 @SpringBootApplication
 public class BbangddoaApplication extends ListenerAdapter {
-
+	static String riot_api_key = System.getenv("riot_api_key");
+	static String bot_token = System.getenv("bot_token");
 	public static void main(String[] args) throws LoginException {
 		SpringApplication.run(BbangddoaApplication.class, args);
 
-		ObjectMapper objectMapper = new ObjectMapper();
+		// ObjectMapper objectMapper = new ObjectMapper();
 
 		Summoner summoner = null;	// DTO
-		String riot_api_key = System.getenv("riot_api_key");
-		// 공백 처리 하드코딩이라서 수정필요
-		String name = "hide on bush";
-		String SummonerName = name.replaceAll(" ", "%20");
-		String requestURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ SummonerName + "?api_key=" + riot_api_key;
 
-		//rest api 로 수정 필요
-		//mvc 패턴 적용 필요
-		
+
+		JDA jda = JDABuilder.createDefault(bot_token).build();
+		// You can also add event listeners to the already built JDA instance
+		// Note that some events may not be received if the listener is added after calling build()
+		// This includes events such as the ReadyEvent
+		jda.getPresence().setStatus(OnlineStatus.ONLINE);
+		jda.addEventListener(new BbangddoaApplication());
+
+	}
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event)
+	{
+		Message msg = event.getMessage();
+		if (msg.getContentRaw().equals("!ping")) {
+			MessageChannel channel = event.getChannel();
+			long time = System.currentTimeMillis();
+			System.out.println("event :" + msg.getContentRaw());
+			event.getChannel().sendMessage("pong!").queue();
+		}
+		else if(msg.getContentRaw().equals("!전적검색")) {
+			String name = "hide on bush";
+			String SummonerName = name.replaceAll(" ", "%20");
+			String requestURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+			searchSummonerInfo(riot_api_key,"vtz",requestURL);
+		}
+		if (event.isFromType(ChannelType.PRIVATE)) {
+			System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+					event.getMessage().getContentDisplay());
+		}
+		else {
+			System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+					event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+					event.getMessage().getContentDisplay());
+		}
+	}
+
+
+	//rest api 로 수정 필요
+	//mvc 패턴 적용 필요
+	public void searchSummonerInfo(String key,String summonerName,String basicRequestURL){
+		String requestURL = basicRequestURL + summonerName + "?api_key=" + key;
 		try {
 			String USER_AGENT = "Mozilla/5.0";
 			URL url = new URL(requestURL);
@@ -67,45 +102,6 @@ public class BbangddoaApplication extends ListenerAdapter {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-
-
-
-
-		String bot_token = System.getenv("bot_token");
-		JDA jda = JDABuilder.createDefault(bot_token).build();
-		// You can also add event listeners to the already built JDA instance
-		// Note that some events may not be received if the listener is added after calling build()
-		// This includes events such as the ReadyEvent
-		jda.getPresence().setStatus(OnlineStatus.ONLINE);
-		jda.addEventListener(new BbangddoaApplication());
-
 	}
-
-	@Override
-	public void onMessageReceived(MessageReceivedEvent event)
-	{
-		Message msg = event.getMessage();
-		if (msg.getContentRaw().equals("!ping"))
-		{
-			MessageChannel channel = event.getChannel();
-			long time = System.currentTimeMillis();
-			System.out.println("event :" + msg.getContentRaw());
-			event.getChannel().sendMessage("pong!").queue();
-		}
-
-		if (event.isFromType(ChannelType.PRIVATE))
-		{
-			System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-					event.getMessage().getContentDisplay());
-		}
-		else
-		{
-			System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-					event.getTextChannel().getName(), event.getMember().getEffectiveName(),
-					event.getMessage().getContentDisplay());
-		}
-	}
-
 
 }
