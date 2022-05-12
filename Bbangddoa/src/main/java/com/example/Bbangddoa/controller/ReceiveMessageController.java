fb35fb550980941d1a0bdffa7cc4d1b3c360f8ad
gnsals0904 <gnsals0904@naver.com>
2022-05-13 00:01:04 +0900

    successed in implementing summoner champion mastery system

--- Bbangddoa/src/main/java/com/example/Bbangddoa/controller/ReceiveMessageController.java
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/controller/ReceiveMessageController.java
@@ -1,9 +1,10 @@
 package com.example.Bbangddoa.controller;
 
-import com.example.Bbangddoa.domain.ChampionMasteryScore;
+import com.example.Bbangddoa.domain.ChampionMastery;
 import com.example.Bbangddoa.domain.LeagueEntry;
 import com.example.Bbangddoa.domain.Summoner;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
@@ -15,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -50,6 +52,7 @@ public class ReceiveMessageController extends ListenerAdapter {
                 System.out.println("summonerName : "+ summonerName);
                 Summoner summonerInfo = null;
                 List<LeagueEntry> summonerLeagueInfo = null;
+                List<ChampionMastery> championMasteries = null;
                 String requestURL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
                 summonerInfo = searchSummonerInfo(riot_api_key, requestURL,summonerName);
                 requestURL = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/";
@@ -57,6 +60,8 @@ public class ReceiveMessageController extends ListenerAdapter {
                 requestURL = "https://kr.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/";
                 int championScore = 0;
                 championScore = Integer.parseInt(searchSummonerScore(riot_api_key, requestURL, summonerInfo.getId()));
+                requestURL = "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/";
+                championMasteries = searchChampionMastery(riot_api_key, requestURL, summonerInfo.getId());
                 System.out.println("championScore :"+championScore );
                 embedBuilder.setTitle("전적 검색");
                 embedBuilder.setColor(new Color(0x44c0e9));
@@ -131,9 +136,15 @@ public class ReceiveMessageController extends ListenerAdapter {
                 else {
                     embedBuilder.addField("뉴비판독기",""+championScore+"점으로 롤이 이제 숨쉬는 것만큼 쉬운 경지에요. 혹시 프로게이머?",false);
                 }
-
-
+                List<String> masteryChampionName = new ArrayList<>();
+                System.out.println("championName :" +getChampionName((championMasteries.get(0).getChampionId()))+getChampionName((championMasteries.get(1).getChampionId()))+getChampionName((championMasteries.get(2).getChampionId())));
+                masteryChampionName.add(getChampionName((championMasteries.get(0).getChampionId())).toString());
+                masteryChampionName.add(getChampionName((championMasteries.get(1).getChampionId())).toString());
+                masteryChampionName.add(getChampionName((championMasteries.get(2).getChampionId())).toString());
+                embedBuilder.addField("아래의 챔피언들에 능숙하시네요!",masteryChampionName.get(0)+"\t"+championMasteries.get(0).getChampionPoints()+"점\n"+masteryChampionName.get(1)+"\t"+championMasteries.get(1).getChampionPoints()+"점\n"+masteryChampionName.get(2)+"\t"+championMasteries.get(2).getChampionPoints()+"점",true);
                 event.getChannel().sendMessage(embedBuilder.build()).queue();
+
+
             }
             if(msg_array[0].equalsIgnoreCase("clear")) {
                 msg.delete().queue();
@@ -177,6 +188,37 @@ public class ReceiveMessageController extends ListenerAdapter {
                     event.getMessage().getContentDisplay());
         }
     }
+
+    public List<ChampionMastery> searchChampionMastery(String key, String basicRequestURL, String summonerEncryptedId){
+        String requestURL = basicRequestURL + summonerEncryptedId + "?api_key=" + key;
+        List<ChampionMastery> championMasteries = null;
+        try {
+            String USER_AGENT = "Mozilla/5.0";
+            URL url = new URL(requestURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET"); // optional default is GET
+            con.setRequestProperty("User-Agent", USER_AGENT); // add request header
+            int responseCode = con.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close(); // print result
+            System.out.println("searchChampionMastery HTTP 응답 코드 : " + responseCode);
+            System.out.println("HTTP body : " + response.toString());
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            championMasteries = objectMapper.readValue(response.toString(), new TypeReference<List<ChampionMastery>>() {});
+            System.out.println("champion id list :" + championMasteries.get(0).getChampionId()+", "+championMasteries.get(1).getChampionId()+", "+championMasteries.get(2).getChampionId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return championMasteries;
+    }
+
+
 
     public String searchSummonerScore(String key, String basicRequestURL, String summonerEncryptedId){
         String requestURL = basicRequestURL + summonerEncryptedId + "?api_key=" + key;
@@ -262,5 +304,330 @@ public class ReceiveMessageController extends ListenerAdapter {
             e.printStackTrace();
         }
         return leagueEntry;
+    }
+
+    public String getChampionName(long id){
+        switch((int) id) {
+            case 266:
+                return "Aatrox";
+            case 412:
+                return "Thresh";
+            case 23:
+                return "Tryndamere";
+            case 79:
+                return "Gragas";
+            case 69:
+                return "Cassiopeia";
+            case 136:
+                return "Aurelion Sol";
+            case 13:
+                return "Ryze";
+            case 78:
+                return "Poppy";
+            case 14:
+                return "Sion";
+            case 1:
+                return "Annie";
+            case 202:
+                return "Jhin";
+            case 43:
+                return "Karma";
+            case 111:
+                return "Nautilus";
+            case 240:
+                return "Kled";
+            case 99:
+                return "Lux";
+            case 103:
+                return "Ahri";
+            case 2:
+                return "Olaf";
+            case 112:
+                return "Viktor";
+            case 34:
+                return "Anivia";
+            case 27:
+                return "Singed";
+            case 86:
+                return "Garen";
+            case 127:
+                return "Lissandra";
+            case 57:
+                return "Maokai";
+            case 25:
+                return "Morgana";
+            case 28:
+                return "Evelynn";
+            case 105:
+                return "Fizz";
+            case 74:
+                return "Heimerdinger";
+            case 238:
+                return "Zed";
+            case 68:
+                return "Rumble";
+            case 82:
+                return "Mordekaiser";
+            case 37:
+                return "Sona";
+            case 96:
+                return "Kog'Maw";
+            case 55:
+                return "Katarina";
+            case 117:
+                return "Lulu";
+            case 22:
+                return "Ashe";
+            case 30:
+                return "Karthus";
+            case 12:
+                return "Alistar";
+            case 122:
+                return "Darius";
+            case 67:
+                return "Vayne";
+            case 110:
+                return "Varus";
+            case 77:
+                return "Udyr";
+            case 89:
+                return "Leona";
+            case 126:
+                return "Jayce";
+            case 134:
+                return "Syndra";
+            case 80:
+                return "Pantheon";
+            case 92:
+                return "Riven";
+            case 121:
+                return "Kha'Zix";
+            case 42:
+                return "Corki";
+            case 268:
+                return "Azir";
+            case 51:
+                return "Caitlyn";
+            case 76:
+                return "Nidalee";
+            case 85:
+                return "Kennen";
+            case 3:
+                return "Galio";
+            case 45:
+                return "Veigar";
+            case 432:
+                return "Bard";
+            case 150:
+                return "Gnar";
+            case 90:
+                return "Malzahar";
+            case 104:
+                return "Graves";
+            case 254:
+                return "Vi";
+            case 10:
+                return "Kayle";
+            case 39:
+                return "Irelia";
+            case 64:
+                return "Lee Sin";
+            case 420:
+                return "Illaoi";
+            case 60:
+                return "Elise";
+            case 106:
+                return "Volibear";
+            case 20:
+                return "Nunu";
+            case 4:
+                return "Twisted Fate";
+            case 24:
+                return "Jax";
+            case 102:
+                return "Shyvana";
+            case 429:
+                return "Kalista";
+            case 36:
+                return "Dr. Mundo";
+            case 427:
+                return "Ivern";
+            case 131:
+                return "Diana";
+            case 223:
+                return "Tahm Kench";
+            case 63:
+                return "Brand";
+            case 113:
+                return "Sejuani";
+            case 8:
+                return "Vladimir";
+            case 154:
+                return "Zac";
+            case 421:
+                return "Rek'Sai";
+            case 133:
+                return "Quinn";
+            case 84:
+                return "Akali";
+            case 163:
+                return "Taliyah";
+            case 18:
+                return "Tristana";
+            case 120:
+                return "Hecarim";
+            case 15:
+                return "Sivir";
+            case 236:
+                return "Lucian";
+            case 107:
+                return "Rengar";
+            case 19:
+                return "Warwick";
+            case 72:
+                return "Skarner";
+            case 54:
+                return "Malphite";
+            case 157:
+                return "Yasuo";
+            case 101:
+                return "Xerath";
+            case 17:
+                return "Teemo";
+            case 75:
+                return "Nasus";
+            case 58:
+                return "Renekton";
+            case 119:
+                return "Draven";
+            case 35:
+                return "Shaco";
+            case 50:
+                return "Swain";
+            case 91:
+                return "Talon";
+            case 40:
+                return "Janna";
+            case 115:
+                return "Ziggs";
+            case 245:
+                return "Ekko";
+            case 61:
+                return "Orianna";
+            case 114:
+                return "Fiora";
+            case 9:
+                return "Fiddlesticks";
+            case 31:
+                return "Cho'Gath";
+            case 33:
+                return "Rammus";
+            case 7:
+                return "LeBlanc";
+            case 16:
+                return "Soraka";
+            case 26:
+                return "Zilean";
+            case 56:
+                return "Nocturne";
+            case 222:
+                return "Jinx";
+            case 83:
+                return "Yorick";
+            case 6:
+                return "Urgot";
+            case 203:
+                return "Kindred";
+            case 21:
+                return "Miss Fortune";
+            case 62:
+                return "Wukong";
+            case 53:
+                return "Blitzcrank";
+            case 98:
+                return "Shen";
+            case 201:
+                return "Braum";
+            case 5:
+                return "Xin Zhao";
+            case 29:
+                return "Twitch";
+            case 11:
+                return "Master Yi";
+            case 44:
+                return "Taric";
+            case 32:
+                return "Amumu";
+            case 41:
+                return "Gangplank";
+            case 48:
+                return "Trundle";
+            case 38:
+                return "Kassadin";
+            case 161:
+                return "Vel'Koz";
+            case 143:
+                return "Zyra";
+            case 267:
+                return "Nami";
+            case 59:
+                return "Jarvan IV";
+            case 81:
+                return "Ezreal";
+            case 164:
+                return "Camile";
+            case 498:
+                return "Xayah";
+            case 497:
+                return "Rakan";
+            case 141:
+                return "Kayn";
+            case 516:
+                return "Ornn";
+            case 142:
+                return "Zoe";
+            case 145:
+                return "Kaisa";
+            case 555:
+                return "Pyke";
+            case 518:
+                return "Neeko";
+            case 517:
+                return "Sylas";
+            case 350:
+                return "Yuumi";
+            case 246:
+                return "Qiyana";
+            case 235:
+                return "Senna";
+            case 523:
+                return "Aphelios";
+            case 876:
+                return "Lillia";
+            case 875:
+                return "Sett";
+            case 777:
+                return "Yone";
+            case 360:
+                return "Samira";
+            case 147:
+                return "Seraphine";
+            case 526:
+                return "Rell";
+            case 234:
+                return "Viego";
+            case 887:
+                return "Gwen";
+            case 166:
+                return "Akshan";
+            case 711:
+                return "Vex";
+            case 221:
+                return "Zeri";
+            case 888:
+                return "Renata Glasc";
+            default:
+                return "error";
+        }
     }
 }
---
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/domain/ChampionMastery.java
@@ -0,0 +1,22 @@
+package com.example.Bbangddoa.domain;
+
+public class ChampionMastery {
+    private long championId;
+    private int championPoints;
+
+    public long getChampionId() {
+        return championId;
+    }
+
+    public void setChampionId(long championId) {
+        this.championId = championId;
+    }
+
+    public int getChampionPoints() {
+        return championPoints;
+    }
+
+    public void setChampionPoints(int championPoints) {
+        this.championPoints = championPoints;
+    }
+}
