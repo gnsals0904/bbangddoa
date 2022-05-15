43b4aab67e002d6f2a7c6139ecb3c69a1a612d4f
gnsals0904 <gnsals0904@naver.com>
2022-05-15 23:45:21 +0900

    add how it does work img

--- Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
+++ Bbangddoa/src/main/java/com/example/Bbangddoa/BbangddoaApplication.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,17 +19,10 @@ public class BbangddoaApplication extends ListenerAdapter {
 		SpringApplication.run(BbangddoaApplication.class, args);
 		JDABuilder builder = JDABuilder.createDefault(bot_token);
 		builder.setActivity(Activity.of(Activity.ActivityType.DEFAULT,"빵또아는 빵안에 아이스크림이 들어가 있다고 주장하는 중"));
-		// JDA jda = JDABuilder.createDefault(bot_token).build();
 
-		// You can also0 add event listeners to the already built JDA instance
-		// Note that some events may not be received if the listener is added after calling build()
-		// This includes events such as the ReadyEvent
 		JDA jda = builder.build();
-
-
 		jda.getPresence().setStatus(OnlineStatus.ONLINE);
 		jda.addEventListener(new ReceiveMessageController());
-
 
 	}
 
